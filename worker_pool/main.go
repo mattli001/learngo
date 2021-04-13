e233c8b454e8db189c21fa22a139f2b5e6804eeb
Matt Lee <mattlee@qnap.com>
2021-04-13 15:21:43 +0800

    feat: concurrent worker pool example

---
+++ worker_pool/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/mattli001/learngo/worker_pool/dispatcher"
+	"github.com/mattli001/learngo/worker_pool/worker"
+)
+
+func init() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+}
+
+func main() {
+	start := time.Now()
+	dd := dispatcher.New(2).Start()
+
+	// terms := []int{1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3,
+	// 	4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3,
+	// 	4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3,
+	// 	4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3,
+	// 	4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3,
+	// 	4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3,
+	// 	4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3,
+	// 	4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3,
+	// 	4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3,
+	// 	4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3,
+	// 	4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3,
+	// 	4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3,
+	// 	4, 1, 2, 3, 4}
+	terms := []int{1, 2, 3, 4, 5}
+	for i := range terms {
+		log.Printf("Submit JobID: %d ... start\n", i)
+		dd.Submit(worker.Job{
+			ID:        i,
+			Name:      fmt.Sprintf("JobID::%d", i),
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+		})
+		log.Printf("Submit JobID: %d ... done\n", i)
+	}
+	end := time.Now()
+	log.Print(end.Sub(start).Seconds())
+	log.Print("exit")
+}
---
+++ worker_pool/worker/worker.go
@@ -0,0 +1,69 @@
+package worker
+
+import (
+	"log"
+	"time"
+)
+
+// Job represents a single entity that should be processed.
+// For example a struct that should be saved to database
+type Job struct {
+	ID        int
+	Name      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type JobChannel chan Job
+type JobQueue chan chan Job
+
+// Worker is a a single processor. Typically its possible to
+// start multiple workers for better throughput
+type Worker struct {
+	ID      int           // id of the worker
+	JobChan JobChannel    // a channel to receive single unit of work
+	Queue   JobQueue      // shared between all workers.
+	Quit    chan struct{} // a channel to quit working
+}
+
+func New(ID int, JobChan JobChannel, Queue JobQueue, Quit chan struct{}) *Worker {
+	return &Worker{
+		ID:      ID,
+		JobChan: JobChan,
+		Queue:   Queue,
+		Quit:    Quit,
+	}
+}
+
+func (wr *Worker) Start() {
+	// c := &http.Client{Timeout: time.Millisecond * 15000}
+	go func() {
+		for {
+			// when available, put the JobChan again on the JobPool
+			// and wait to receive a job
+			wr.Queue <- wr.JobChan
+			select {
+			case job := <-wr.JobChan:
+				// when a job is received, process
+				callApi(job.ID, wr.ID)
+			case <-wr.Quit:
+				// a signal on this channel means someone triggered
+				// a shutdown for this worker
+				close(wr.JobChan)
+				return
+			}
+		}
+	}()
+}
+
+// stop closes the Quit channel on the worker.
+func (wr *Worker) Stop() {
+	close(wr.Quit)
+}
+
+func callApi(num, id int) {
+	log.Printf("num %d, id %d\n", num, id)
+	time.Sleep(3 * time.Second)
+	log.Printf("callApi num %d, id %d exit\n", num, id)
+	//log.Printf("%d  :: ok", id)
+}
